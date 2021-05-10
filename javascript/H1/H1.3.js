22795acc4d5254f78b067dbdaecd0e80a43edcf2
JayGupGup <71427304+JayGupGup@users.noreply.github.com>
2021-05-10 22:53:19 +0200

    Add files via upload

---
+++ javascript/H1/H1.3.js
@@ -0,0 +1,8 @@
+var mijnauto = {
+    merk: "BMW",
+    type: "E46",
+    aantalwielen: 4,
+    kleur: "zwart"
+}
+
+console.log(mijnauto.kleur)
---
+++ javascript/H2/H2.1.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("pic1").src = "luffy 1.png"
+document.getElementById("pic2").src = "luffy 2.gif"
+
